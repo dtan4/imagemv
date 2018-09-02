b47a35c1c928083553f29d8121836cc5a2bf885e
Daisuke Fujita <dtanshi45@gmail.com>
2018-09-02 22:06:36 +0900

    Create CLI package

---
+++ cli/cli.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"fmt"
+	"io"
+)
+
+// CLI represents CLI object
+type CLI struct {
+	stdout io.Writer
+	stderr io.Writer
+}
+
+// New creates CLI object
+func New(stdout, stderr io.Writer) *CLI {
+	return &CLI{
+		stdout: stdout,
+		stderr: stderr,
+	}
+}
+
+// Run executes main command logic
+func (cli *CLI) Run(args []string) error {
+	fmt.Fprintln(cli.stdout, "imagemv")
+	return nil
+}
--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	cli "github.com/dtan4/imagemv/cli"
+	"os"
 )
 
 func main() {
-	fmt.Println("imagemv")
+	c := cli.New(os.Stdout, os.Stderr)
+
+	if err := c.Run(os.Args); err != nil {
+		if os.Getenv("DEBUG") == "1" {
+			fmt.Fprintf(os.Stderr, "%+v\n", err)
+		} else {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		os.Exit(1)
+	}
 }
